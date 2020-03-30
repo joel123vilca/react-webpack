1e50223f06db546614573332c9ed31ffb9bdc5ae
joel123vilca <joevil23tjq@gmail.com>
2020-03-29 21:03:15 -0500

    material ui i

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import App from './components/App';
 render(
     <div>
-        <h1>app</h1>
         <App></App>
     </div>,
     document.getElementById('app')
